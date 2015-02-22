eac65ee46f5dd6553135d17907147889a4f216d9
fcbruce <fcbruce8964@gmail.com>
2015-02-22 15:45:04 +0800

    模拟乱搞

--- code/UVA/489.cc
+++ code/UVA/489.cc
@@ -27,64 +27,65 @@ int cnt,total,error;
 
 int main()
 {
-	#ifndef ONLINE_JUDGE
-		freopen("t","r",stdin);
-	#endif
+#ifndef ONLINE_JUDGE
+	freopen("/home/fcbruce/code/t","r",stdin);
+#endif
 
 	int r;
+
 	while (scanf("%d",&r)&&(~r))
+  {
+    scanf("%s%s",str1,str2);
+
+    cnt=total=error=0;
+
+    int l1=strlen(str1),l2=strlen(str2);
+
+    memset(alp,0,sizeof(alp));
+
+    for (int i=0;i<l1;i++)
     {
-        scanf("%s%s",str1,str2);
-
-        cnt=total=error=0;
-
-        int l1=strlen(str1),l2=strlen(str2);
-
-        memset(alp,0,sizeof(alp));
-
-        for (int i=0;i<l1;i++)
-        {
-            if (alp[str1[i]-'a']==0)
-            {
-                total++;
-                alp[str1[i]-'a']=1;
-            }
-        }
-
-        for (int i=0;i<l2;i++)
-        {
-            if (alp[str2[i]-'a']==-1)
-                continue;
-
-            if (alp[str2[i]-'a']==1)
-            {
-                alp[str2[i]-'a']=-1;
-                cnt++;
-                if (cnt==total)
-                    break;
-            }
-            else
-            {
-                error++;
-                if (error==7) break;
-            }
-        }
-
-        printf("Round %d\n",r);
-        if (error==7)
-        {
-            puts("You lose.");
-        }
-        else
-        {
-            if (cnt==total)
-                puts("You win.");
-            else
-                puts("You chickened out.");
-        }
-
-
+      if (alp[str1[i]-'a']==0)
+      {
+        total++;
+        alp[str1[i]-'a']=1;
+      }
     }
+
+    for (int i=0;i<l2;i++)
+    {
+      if (alp[str2[i]-'a']==-1)
+        continue;
+
+      if (alp[str2[i]-'a']==1)
+      {
+        alp[str2[i]-'a']=-1;
+        cnt++;
+        if (cnt==total)
+          break;
+      }
+      else
+      {
+        error++;
+        if (error==7) break;
+      }
+    }
+
+    printf("Round %d\n",r);
+    if (error==7)
+    {
+      puts("You lose.");
+    }
+    else
+    {
+      if (cnt==total)
+        puts("You win.");
+      else
+        puts("You chickened out.");
+    }
+
+
+  }
 
 
 	return 0;
