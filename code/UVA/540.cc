241005aa0f91d9848a36b697c03f66b8a990420a
fcbruce <fcbruce8964@gmail.com>
2015-02-25 16:24:32 +0800

    stl,list

--- code/UVA/540.cc
+++ code/UVA/540.cc
@@ -28,71 +28,71 @@ int last[1001];
 
 int main()
 {
-    #ifndef ONLINE_JUDGE
-        freopen("/home/fcbruce/文档/code/t","r",stdin);
-    #endif // ONLINE_JUDGE
+  #ifndef ONLINE_JUDGE
+    freopen("/home/fcbruce/文档/code/t","r",stdin);
+  #endif // ONLINE_JUDGE
 
-    int n,m,x,cnt,tt=0,k;
+  int n,m,x,cnt,tt=0,k;
 
-    while (scanf("%d",&n),n)
+  while (scanf("%d",&n),n)
+  {
+    printf("Scenario #%d\n",++tt);
+    memset(Hash,-1,sizeof(Hash));
+    memset(last,-1,sizeof(last));
+    cnt=1;
+
+    for (int i=1;i<=n;i++)
     {
-        printf("Scenario #%d\n",++tt);
-        memset(Hash,-1,sizeof(Hash));
-        memset(last,-1,sizeof(last));
-        cnt=1;
-
-        for (int i=1;i<=n;i++)
-        {
-            scanf("%d",&m);
-            for (int j=0;j<m;j++)
-            {
-                scanf("%d",&x);
-                Hash[x]=i;
-            }
-        }
-
-        list<int> l,t;
-        list<int>::iterator its[1001],it;
-
-        while (true)
-        {
-            scanf("%s",cmd);
-           // puts(cmd);
-            if (strcmp(cmd,"STOP")==0) break;
-
-           // puts(cmd);
-
-            if (cmd[0]=='E')
-            {
-                scanf("%d",&x);
-                int k=Hash[x];
-                if (last[k]==-1)
-                {
-                    its[k]=l.end();
-                    its[k]--;
-                }
-
-                its[k]++;
-                its[k]=l.insert(its[k],x);
-                last[k]=x;
-                continue;
-            }
-            else
-            {
-                x=l.front();
-                l.pop_front();
-                k=Hash[x];
-                printf("%d\n",x);
-                if (last[k]==x)
-                {
-                    last[k]=-1;
-                }
-            }
-        }
-
-        puts("");
-
+      scanf("%d",&m);
+      for (int j=0;j<m;j++)
+      {
+        scanf("%d",&x);
+        Hash[x]=i;
+      }
     }
 
-    return 0;
+    list<int> l,t;
+    list<int>::iterator its[1001],it;
+
+    while (true)
+    {
+      scanf("%s",cmd);
+       // puts(cmd);
+      if (strcmp(cmd,"STOP")==0) break;
+
+       // puts(cmd);
+
+      if (cmd[0]=='E')
+      {
+        scanf("%d",&x);
+        int k=Hash[x];
+        if (last[k]==-1)
+        {
+          its[k]=l.end();
+          its[k]--;
+        }
+
+        its[k]++;
+        its[k]=l.insert(its[k],x);
+        last[k]=x;
+        continue;
+      }
+      else
+      {
+        x=l.front();
+        l.pop_front();
+        k=Hash[x];
+        printf("%d\n",x);
+        if (last[k]==x)
+        {
+          last[k]=-1;
+        }
+      }
+    }
+
+    puts("");
+
+  }
+
+  return 0;
 }
