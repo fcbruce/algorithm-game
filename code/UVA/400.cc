02d730ed43c996070855b8b57e1c4a72bf5e48cb
fcbruce <fcbruce8964@gmail.com>
2015-02-25 16:25:53 +0800

    模拟乱搞

--- code/UVA/400.cc
+++ code/UVA/400.cc
@@ -26,40 +26,40 @@ char f[111];
 
 int main()
 {
-	#ifndef ONLINE_JUDGE
-		freopen("t","r",stdin);
-	#endif
+  #ifndef ONLINE_JUDGE
+    freopen("t","r",stdin);
+  #endif
 
-	while (scanf("%d",&n)==1)
+  while (scanf("%d",&n)==1)
+  {
+    MAXSIZE=0;
+    for (int i=0;i<n;i++)
     {
-        MAXSIZE=0;
-        for (int i=0;i<n;i++)
-        {
-            scanf("%s",f);
-            filename[i]=f;
-            MAXSIZE=max(MAXSIZE,(int)filename[i].size());
-        }
-        MAXSIZE=min(60,MAXSIZE+2);
-        numline=60/MAXSIZE;
-        row=n/numline;
-        if (n%numline)  row++;
-        sort(filename,filename+n);
-        puts("------------------------------------------------------------");
-        for (int i=0;i<row;i++)
-        {
-            for (int j=0;j<numline;j++)
-            {
-                if (i+j*row>=n) break;
-                printf("%s",filename[i+j*row].c_str());
-                for (int k=filename[i+j*row].size();k<MAXSIZE;k++)
-                    printf(" ");
-            }
-            puts("");
-        }
-
-
+      scanf("%s",f);
+      filename[i]=f;
+      MAXSIZE=max(MAXSIZE,(int)filename[i].size());
+    }
+    MAXSIZE=min(60,MAXSIZE+2);
+    numline=60/MAXSIZE;
+    row=n/numline;
+    if (n%numline)  row++;
+    sort(filename,filename+n);
+    puts("------------------------------------------------------------");
+    for (int i=0;i<row;i++)
+    {
+      for (int j=0;j<numline;j++)
+      {
+        if (i+j*row>=n) break;
+        printf("%s",filename[i+j*row].c_str());
+        for (int k=filename[i+j*row].size();k<MAXSIZE;k++)
+          printf(" ");
+      }
+      puts("");
     }
 
 
-	return 0;
+  }
+
+
+  return 0;
 }
