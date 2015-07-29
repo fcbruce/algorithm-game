24844385ba494b98b4c5a4fee4b4be4a12ff4232
fcbruce <fcbruce8964@gmail.com>
2015-07-29 21:22:05 +0800

    KMP,计数,dp

---
+++ code/HDU/3336_Countthestring/Main.java
@@ -0,0 +1,149 @@
+/*
+ *
+ * Author : fcbruce <fcbruce8964@gmail.com>
+ *
+ * Time : Mon 27 Jul 2015 04:53:57 PM CST
+ *
+ */
+import java.util.*;
+import java.math.*;
+import java.io.*;
+public class Main {
+  InputStream in;
+  PrintWriter out;
+
+  int kmp(char []s,int n) {
+
+    int []nex = new int [n];
+    int []cnt = new int [n];
+    nex[0]=-1;
+    Arrays.fill(cnt,1);
+
+    int res=1;
+    
+    for (int i=1,j=-1;i<n;i++) {
+      while (j>-1 && s[j+1]!=s[i]) j=nex[j];
+      if (s[j+1]==s[i]) j++;
+      nex[i]=j;
+      if (j>-1) cnt[i]+=cnt[j];
+      cnt[i]%=10007;
+      res+=cnt[i];
+      res%=10007;
+    }
+
+    return res;
+  }
+  
+  void solve() {
+    
+    int T_T = nextInt();
+
+    while (T_T-->0) {
+      int n = nextInt();
+      char []s = nextString(n);
+
+      out.println(kmp(s,n));
+    }
+    
+  }
+  
+  void run() throws Exception {
+    in = System.in;
+    out = new PrintWriter(System.out);
+    solve();
+    out.flush();
+  }
+  
+  public static void main(String[] args) throws Exception { new Main().run(); }
+  
+  private byte[] inbuf = new byte[1024];
+  private int lenbuf = 0, ptrbuf = 0;
+  
+  private int readByte() {
+    if (lenbuf == -1) throw new InputMismatchException();
+    if (ptrbuf >= lenbuf){
+      ptrbuf = 0;
+      try { lenbuf = in.read(inbuf); } catch (IOException e) { throw new InputMismatchException(); }
+      if (lenbuf <= 0) return -1;
+    }
+    return inbuf[ptrbuf++];
+  }
+  
+  private boolean isSpaceChar(int c) { return !(c >= 33 && c <= 126); }
+  private int skip() { int b; while ((b = readByte()) != -1 && isSpaceChar(b)); return b; }
+  
+  private double nextDouble() { return Double.parseDouble(nextString()); }
+  private char nextChar() { 
+    int b = readByte();
+    if (b==-1) throw new InputMismatchException();
+    else return (char)b;
+  }
+  
+  private String nextString() {
+    int b = skip();
+    if (b == -1) throw new InputMismatchException();
+    StringBuilder sb = new StringBuilder();
+    while (!(isSpaceChar(b))) { // when nextLine, (isSpaceChar(b) && b != ' ')
+      sb.appendCodePoint(b);
+      b = readByte();
+    }
+    return sb.toString();
+  }
+  
+  private char[] nextString(int n) {
+    char[] buf = new char[n];
+    int b = skip(), p = 0;
+    while (p < n && !(isSpaceChar(b))) {
+      buf[p++] = (char)b;
+      b = readByte();
+    }
+    return n == p ? buf : Arrays.copyOf(buf, p);
+  }
+  
+  private int[] nextArray(int n) {
+    int[] a = new int[n];
+    for (int i = 0;i < n;i++) a[i] = nextInt();
+    return a;
+  }
+  
+  private int nextInt() {
+    int num = 0, b;
+    boolean minus = false;
+    while ((b = readByte()) != -1 && !((b >= '0' && b <= '9') || b == '-'));
+    if (b == -1) throw new InputMismatchException();
+    if (b == '-') {
+      minus = true;
+      b = readByte();
+    }
+    
+    while (true) {
+      if (b >= '0' && b <= '9') {
+        num = num * 10 + (b - '0');
+      } else { 
+        return minus ? -num : num;
+      }
+      b = readByte();
+    }
+  }
+  
+  private long nextLong() {
+    long num = 0;
+    int b;
+    boolean minus = false;
+    while ((b = readByte()) != -1 && !((b >= '0' && b <= '9') || b == '-'));
+    if (b == -1) throw new InputMismatchException();
+    if (b == '-'){
+      minus = true;
+      b = readByte();
+    }
+    
+    while (true) {
+      if (b >= '0' && b <= '9') {
+        num = num * 10 + (b - '0');
+      } else {
+        return minus ? -num : num;
+      }
+      b = readByte();
+    }
+  }
+}
